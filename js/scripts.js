ff36f93f26e1e39208835d8800761c45447c742e
ibukamshindi <ibukamshindi@gmail.com>
2020-07-30 11:44:12 +0300

    add functionality scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,123 @@
 $(document).ready(function(){
-  $(".thumbnail1,.thumbnail2,.thumbnail3").mouseenter(function(){
-    $('#definitions').stop().show();
+  $(".thumbnail1").mouseenter(function(){
+    $('#diz').stop().show();
   });
 
-  $(".thumbnail1,.thumbnail1,.thumbnail2  #definitions").mouseleave(function(){
-    if(!$('#definitions').is(':hover')){
-      $('#definitions').hide();
-    };
+  $(".thumbnail1").mouseleave(function(){
+        if(!$('#diz').is(':hover')){
+          $('#diz').hide();
+        };
+      });
+});
+
+$(document).ready(function(){
+  $(".thumbnail2").mouseenter(function(){
+    $('#dev').stop().show();
   });
+
+  $(".thumbnail2").mouseleave(function(){
+        if(!$('#dev').is(':hover')){
+          $('#dev').hide();
+        };
+      });
+});
+
+$(document).ready(function(){
+  $(".thumbnail3").mouseenter(function(){
+    $('#prdct').stop().show();
+  });
+
+  $(".thumbnail3").mouseleave(function(){
+        if(!$('#prdct').is(':hover')){
+          $('#prdct').hide();
+        };
+      });
+});
+
+$(document).ready(function(){
+  $(".l1").mouseover(function(){
+    $(".caption1").show();
+  });
+
+  $(".l1").mouseout(function(){
+    $(".caption1").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".l2").mouseover(function(){
+    $(".caption2").show();
+  });
+
+  $(".l2").mouseout(function(){
+    $(".caption2").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".l3").mouseover(function(){
+    $(".caption3").show();
+  });
+
+  $(".l3").mouseout(function(){
+    $(".caption3").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".l4").mouseover(function(){
+    $(".caption4").show();
+  });
+
+  $(".l4").mouseout(function(){
+    $(".caption4").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".l5").mouseover(function(){
+    $(".caption5").show();
+  });
+
+  $(".l5").mouseout(function(){
+    $(".caption5").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".l6").mouseover(function(){
+    $(".caption6").show();
+  });
+
+  $(".l6").mouseout(function(){
+    $(".caption6").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".l7").mouseover(function(){
+    $(".caption7").show();
+  });
+
+  $(".l7").mouseout(function(){
+    $(".caption7").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".l8").mouseover(function(){
+    $(".caption8").show();
+  });
+
+  $(".l8").mouseout(function(){
+    $(".caption8").hide();
+  });
+
 });
